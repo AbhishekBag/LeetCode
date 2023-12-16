f8c76f7b420753237feb9cd6868a094347457f1a
Abhishek <48788855+AbhishekBag@users.noreply.github.com>
2023-12-16 18:35:08 +0530

    Time: 74 ms (47.08%) | Memory: 41 MB (52.38%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.cs
@@ -0,0 +1,30 @@
+public class Solution {
+    public bool IsPalindrome(string s) {
+        if(s.Length <= 0) {
+            return true;
+        }
+
+        s = CleanString(s.ToLower());
+        // Console.WriteLine($"after cleanup: s: {s}");
+        int n = s.Length;
+        for(int i = 0; i < n / 2; i++) {
+            if(s[i] != s[n - i - 1]) {
+                // Console.WriteLine($"not equal; i: {i}; s[i]: {s[i]}, s[n - i - 1]: {s[n - i - 1]}");
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private string CleanString(string s) {
+        StringBuilder sb = new StringBuilder();
+        foreach(char c in s) {
+            if(c >= 'a' && c <= 'z' || c >= '0' && c <= '9') {
+                sb.Append(c);
+            }
+        }
+
+        return sb.ToString();
+    }
+}
