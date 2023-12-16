f37370baff17d4af899226b97aa0bbd6a78cc1f1
Abhishek <48788855+AbhishekBag@users.noreply.github.com>
2023-12-16 17:04:44 +0530

    Time: 79 ms (63.35%) | Memory: 39.8 MB (68.17%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cs
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cs
@@ -11,10 +11,6 @@
  */
 public class Solution {
     public ListNode MergeTwoLists(ListNode list1, ListNode list2) {
-        // if(list1 == null && list2 == null) {
-        //     return list1;
-        // }
-
         if(list1 == null) {
             return list2;
         }
@@ -24,11 +20,9 @@ public class Solution {
 
         ListNode head = null, tmp = null;
         if(list1.val > list2.val) {
-            // Console.WriteLine($"Added {list2.val} from list2.");
             head = list2;
             list2 = list2.next;
         } else {
-            // Console.WriteLine($"Added {list1.val} from list1.");
             head = list1;
             list1 = list1.next;
         }
@@ -44,11 +38,9 @@ public class Solution {
                 break;
             }
             if(list1.val > list2.val) {
-                // Console.WriteLine($"Added {list2.val} from list2.");
                 tmp.next = list2;
                 list2 = list2.next;
             } else {
-                // Console.WriteLine($"Added {list1.val} from list1.");
                 tmp.next = list1;
                 list1 = list1.next;
             }
