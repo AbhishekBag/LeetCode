0bfd60efbd4f475c0c88867ee07d2d9d24052bea
Abhishek <48788855+AbhishekBag@users.noreply.github.com>
2024-01-26 21:19:59 +0530

    Time: 46 ms (96.65%) | Memory: 42.2 MB (6.57%) - LeetSync

--- 392-is-subsequence/is-subsequence.cs
+++ 392-is-subsequence/is-subsequence.cs
@@ -1,24 +1,46 @@
 public class Solution {
+    int[][] arr;
     public bool IsSubsequence(string s, string t) {
-        return IsSubsequenceHelper(s, t, 0, 0);
-    }
-
-    private bool IsSubsequenceHelper(string s, string t, int p1, int p2) {
-        if(p1 >= s.Length) {
+        if(s.Length == 0) {
             return true;
         }
 
-        if(p2 >= t.Length) {
+        if(t.Length == 0) {
             return false;
         }
 
-        if(s[p1] == t[p2]) {
-            bool choseChar = IsSubsequenceHelper(s, t, p1 + 1, p2 + 1);
-            bool dontChoseChar = IsSubsequenceHelper(s, t, p1, p2 + 1);
-
-            return choseChar || dontChoseChar;
-        } else {
-            return IsSubsequenceHelper(s, t, p1, p2 + 1);
+        arr = new int[s.Length][];
+        for(int i = 0; i < s.Length; i++) {
+            arr[i] = Enumerable.Repeat(-1, t.Length).ToArray();
         }
+
+        return IsSubsequenceHelper(arr, s, t, 0, 0) == 1 ? true : false;
+
+        // return arr[s.Length - 1][t.Length - 1] == 1 ? true : false;
+    }
+
+    private int IsSubsequenceHelper(int[][] arr, string s, string t, int p1, int p2) {
+        if(p1 >= s.Length) {
+            return 1;
+        }
+
+        if(p2 >= t.Length) {
+            return 0;
+        }
+
+        if(arr[p1][p2] != -1) {
+            return arr[p1][p2];
+        }
+
+        if(s[p1] == t[p2]) {
+            int choseChar = IsSubsequenceHelper(arr, s, t, p1 + 1, p2 + 1);
+            int dontChoseChar = IsSubsequenceHelper(arr, s, t, p1, p2 + 1);
+
+            arr[p1][p2] = (choseChar == 1 || dontChoseChar == 1) ? 1 : 0;
+        } else {
+            arr[p1][p2] = IsSubsequenceHelper(arr, s, t, p1, p2 + 1);
+        }
+
+        return arr[p1][p2];
     }
 }
