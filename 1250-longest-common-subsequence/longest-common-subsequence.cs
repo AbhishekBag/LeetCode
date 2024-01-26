60926190b1500b0e733827ae9f6f985b3af44c0c
Abhishek <48788855+AbhishekBag@users.noreply.github.com>
2024-01-27 00:47:14 +0530

    Time: 80 ms (36.91%) | Memory: 49.9 MB (19.59%) - LeetSync

---
+++ 1250-longest-common-subsequence/longest-common-subsequence.cs
@@ -0,0 +1,32 @@
+public class Solution {
+    int[][] arr;
+    public int LongestCommonSubsequence(string text1, string text2) {
+        arr = new int[text1.Length][];
+        for(int i = 0; i < text1.Length; i++) {
+            arr[i] = Enumerable.Repeat(-1, text2.Length).ToArray();
+        }
+
+        return GetSequenceLength(arr, text1, text2, 0, 0);
+    }
+
+    private int GetSequenceLength(int[][] arr, string text1, string text2, int p1, int p2) {
+        if(p1 >= text1.Length || p2 >= text2.Length) {
+            return 0;
+        }
+
+        if(arr[p1][p2] != -1) {
+            return arr[p1][p2];
+        }
+
+        if(text1[p1] != text2[p2]) {
+            int IncreaseT1 = GetSequenceLength(arr, text1, text2, p1 + 1, p2);
+            int IncreaseT2 = GetSequenceLength(arr, text1, text2, p1, p2 + 1);
+
+            arr[p1][p2] = Math.Max(IncreaseT1, IncreaseT2);
+        } else {
+            arr[p1][p2] = 1 + GetSequenceLength(arr, text1, text2, p1 + 1, p2 + 1);
+        }
+
+        return arr[p1][p2];
+    }
+}
