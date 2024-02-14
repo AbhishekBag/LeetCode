255c2392922dcd0a243d148ea906ab67c7bd0414
Abhishek <48788855+AbhishekBag@users.noreply.github.com>
2024-02-14 10:16:08 +0530

    Time: 71 ms (21.58%) | Memory: 41 MB (36.34%) - LeetSync

---
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cs
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode RemoveNthFromEnd(ListNode head, int n) {
+        if(head == null || n == 0) {
+            return head;
+        }
+        if(head.next == null && n == 1) {
+            return null;
+        }
+        var tmp = head;
+        while(tmp != null && n != 0) {
+            tmp = tmp.next;
+            n--;
+        }
+
+        if(tmp == null) {
+            return head.next;
+        }
+
+        var prev = head;
+        var next = head;
+
+        while(tmp != null) {
+            prev = next;
+            next = next.next;
+            tmp = tmp.next;
+        }
+
+        prev.next = next.next;
+
+        return head;
+    }
+}
