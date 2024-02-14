e43df3adc86dab007975259edb911291588a59e3
Abhishek <48788855+AbhishekBag@users.noreply.github.com>
2024-02-14 10:02:50 +0530

    Time: 144 ms (88.61%) | Memory: 55.5 MB (63.03%) - LeetSync

--- 56-merge-intervals/merge-intervals.cs
+++ 56-merge-intervals/merge-intervals.cs
@@ -1,31 +1,23 @@
 public class Solution {
     public int[][] Merge(int[][] intervals) {
-        if(intervals.Length == 1) {
-            return intervals;
-        }
-
-        List<int[]> res = new List<int[]>();
-
-        Array.Sort(intervals, (x, y) => {
-            return x[0].CompareTo(y[0]);
+        Array.Sort(intervals, (a, b) => {
+            return a[0].CompareTo(b[0]);
         });
 
-        //1, 5     5, 6
-
-        var start = intervals[0];
-        bool merged = false;
+        List<int[]> res =  new List<int[]>();
+        var current = intervals[0];
         for(int i = 1; i < intervals.Length; i++) {
-            var second = intervals[i];
-            if(start[1] >= second[0]) {
-                start[0] = Math.Min(start[0], second[0]);
-                start[1] = Math.Max(start[1], second[1]);
+            var next = intervals[i];
+            if(current[1] >= next[0]) {
+                current[0] = Math.Min(current[0], next[0]);
+                current[1] = Math.Max(current[1], next[1]);
             } else {
-                res.Add(start);
-                start = second;
+                res.Add(current);
+                current = next;
             }
         }
 
-        res.Add(start);
+        res.Add(current);
 
         return res.ToArray();
     }
