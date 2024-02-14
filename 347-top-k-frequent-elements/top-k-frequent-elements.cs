36701a53beba8006793f0e490b8791050a9948ea
Abhishek <48788855+AbhishekBag@users.noreply.github.com>
2024-02-14 11:07:14 +0530

    Time: 139 ms (29.20%) | Memory: 50.1 MB (85.95%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cs
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cs
@@ -1,5 +1,6 @@
 public class Solution {
-    public int[] TopKFrequent(int[] nums, int k) {
+    public int[] TopKFrequent(int[] nums, int k) {        
+        List<int> res = new List<int>();
         Dictionary<int,int> frequencyMap = new Dictionary<int, int>();
         foreach(var num in nums) {
             if(!frequencyMap.ContainsKey(num)) {
@@ -9,12 +10,25 @@ public class Solution {
             frequencyMap[num]++;
         }
 
-        var freqList = frequencyMap.Select(x => (x.Key, x.Value)).ToList();
-        freqList.Sort((a, b) => b.Item2.CompareTo(a.Item2));
-        List<int> res = new List<int>();
-        for(int i = 0; i < k; i++) {
-            res.Add(freqList[i].Item1);
+        // Comparer<int>.Create((x, y) => y.CompareTo(x))
+        PriorityQueue<int, int> map = new PriorityQueue<int, int>();
+        foreach(var item in frequencyMap) {
+            map.Enqueue(item.Key, item.Value);
+            if(map.Count > k) {
+                map.Dequeue();
+            }
         }
+
+        for(int i = 0; i < k; i++) {
+            var poped = map.Dequeue();
+            res.Add(poped);
+        }
+
+        // var freqList = frequencyMap.Select(x => (x.Key, x.Value)).ToList();
+        // freqList.Sort((a, b) => b.Item2.CompareTo(a.Item2));
+        // for(int i = 0; i < k; i++) {
+        //     res.Add(freqList[i].Item1);
+        // }
 
         return res.ToArray();
     }
