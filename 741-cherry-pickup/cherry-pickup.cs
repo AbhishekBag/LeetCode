682fd7751a0ae48c740af8ce85a05625d503298f
Abhishek <48788855+AbhishekBag@users.noreply.github.com>
2024-02-19 02:21:35 +0530

    Time: 242 ms (17.14%) | Memory: 143.9 MB (5.72%) - LeetSync

---
+++ 741-cherry-pickup/cherry-pickup.cs
@@ -0,0 +1,65 @@
+public class Solution {
+    private int[][][][] dp;
+    private int[][] grid;
+    private int n;
+    public int CherryPickup(int[][] grid) {
+        n = grid.Length;
+        this.grid = grid;
+        dp = new int[n][][][];
+        for(int i = 0; i < n; i++) {
+            dp[i] = new int[n][][];
+            for(int j = 0; j < n; j++) {
+                dp[i][j] = new int[n][];
+                for(int k = 0; k < n; k++) {
+                    dp[i][j][k] = Enumerable.Repeat(-1, n).ToArray();
+                }
+            }
+        }
+
+        var maxCherries = GetMaxCherry(0, 0, 0, 0);
+        if(maxCherries < 0) {
+            return 0;
+        }
+
+        return maxCherries;
+    }
+
+    private int GetMaxCherry(int r1, int c1, int r2, int c2) {
+        if(r1 >= n || r2 >= n || c1 >= n || c2 >= n || grid[r1][c1] == -1 || grid[r2][c2] == -1) {
+            return Int32.MinValue;
+        }
+
+        if(dp[r1][c1][r2][c2] != -1) {
+            return dp[r1][c1][r2][c2];
+        }
+
+        int v1 = grid[r1][c1];
+        int v2 = grid[r2][c2];
+        int cherries = 0;
+
+        if(r1 == r2 && c1 == c2) {
+            cherries += grid[r1][c1];
+            if(r1 == n - 1 && c1 == n -1) {
+                dp[r1][c1][r2][c2] = grid[r1][c1];
+                return dp[r1][c1][r2][c2];
+            }
+            grid[r1][c1] = 0;
+        } else {
+            cherries += grid[r1][c1] + grid[r2][c2];
+            grid[r1][c1] = 0;
+            grid[r2][c2] = 0;
+        }
+
+        int p1 = GetMaxCherry(r1 + 1, c1, r2 + 1, c2);
+        int p2 = GetMaxCherry(r1 + 1, c1, r2, c2 + 1);
+        int p3 = GetMaxCherry(r1, c1 + 1, r2, c2 + 1);
+        int p4 = GetMaxCherry(r1, c1 + 1, r2 + 1, c2);
+
+        grid[r1][c1] = v1;
+        grid[r2][c2] = v2;
+
+        dp[r1][c1][r2][c2] = cherries + Math.Max(Math.Max(p1, p2), Math.Max(p3, p4));
+
+        return dp[r1][c1][r2][c2];
+    }
+}
