071231be60c4fd86173d8b08f77d212526bca3aa
Abhishek <48788855+AbhishekBag@users.noreply.github.com>
2024-02-19 15:49:16 +0530

    Time: 227 ms (83.60%) | Memory: 63.5 MB (69.08%) - LeetSync

--- 744-network-delay-time/network-delay-time.cs
+++ 744-network-delay-time/network-delay-time.cs
@@ -2,7 +2,7 @@ public class Solution {
     public int NetworkDelayTime(int[][] times, int n, int k) {
         int[] d = new int[n + 1];
         Queue<int> currentSet = new Queue<int>();
-        HashSet<int> visited = new HashSet<int>();
+        // HashSet<int> visited = new HashSet<int>();
         Dictionary<int, List<(int destination, int weight)>> adjList = new Dictionary<int, List<(int destination, int weight)>>();
 
         for(int i = 0; i <= n; i++) {
@@ -15,9 +15,9 @@ public class Solution {
         while(currentSet.Count() > 0) {
             var currentNode = currentSet.Dequeue();
 
-            if(!visited.Contains(currentNode)) {
-                visited.Add(currentNode);                               
-            }
+            // if(!visited.Contains(currentNode)) {
+            //     visited.Add(currentNode);                               
+            // }
 
             if(adjList.ContainsKey(currentNode)){
                 var destNodeItems = adjList[currentNode];
