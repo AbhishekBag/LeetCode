23c06b12d10a4d530e260613b688747b33521f8b
Abhishek <48788855+AbhishekBag@users.noreply.github.com>
2024-02-20 01:27:34 +0530

    Time: 480 ms (49.24%) | Memory: 66.5 MB (52.29%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cs
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cs
@@ -1,73 +1,93 @@
 public class Solution {
-    public int MinCostConnectPoints(int[][] points) {
+    public int MinCostConnectPoints(int[][] points)
+    {
         int n = points.Length;
-        PriorityQueue<Edge, int> pq = new PriorityQueue<Edge, int>();
+        int cost = 0;
+        List<Edge> Edges = new List<Edge>();
+        DisjointSet ds = new DisjointSet(n);
 
-        for(int i = 0; i < n; i++) {
-            for(int j = i + 1; j < n;  j++) {
-                int d = GetDistance(points, i, j);
-                pq.Enqueue(new Edge(i, j, d), d);
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                var weight = Math.Abs(points[i][0] - points[j][0]) + Math.Abs(points[i][1] - points[j][1]);
+                Edges.Add(new Edge(i, j, weight));
             }
         }
 
-        UnionFind uf = new UnionFind(n);
-        int minCost = 0;
-        int vCount = 0;
-        while(vCount <= n - 1 && pq.Count > 0) {
-            Edge pEdge = pq.Dequeue();
-            if(uf.Union(pEdge.v1, pEdge.v2)) {
-                minCost += pEdge.d;
-                vCount++;
+        Edges.Sort((x, y) => {
+            return x.w.CompareTo(y.w);
+        });
+
+        foreach(var edge in Edges) {
+            if(!ds.AreConnected(edge.v1, edge.v2)) {
+                cost += edge.w;
+                ds.Union(edge.v1, edge.v2);
             }
         }
 
-        return minCost;
-    }
-
-    private int GetDistance(int[][] points, int x, int y) {
-        return Math.Abs(points[x][0] - points[y][0]) + Math.Abs(points[x][1] - points[y][1]);
+        return cost;
     }
 }
 
 public class Edge {
-    public int v1;
-    public int v2;
-    public int d;
+    public int v1 { get; set; }
+    public int v2 { get; set; }
+    public int w { get; set; }
 
-    public Edge(int _a, int _b, int _d) {
+    public Edge(int _a, int _b, int _w) {
         v1 = _a;
         v2 = _b;
-        d = _d;
+        w = _w;
+    }
+
+    public override int GetHashCode()
+    {
+        return v1.GetHashCode() ^ v2.GetHashCode();
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if(obj is Edge)
+        {
+            var other = (Edge)obj;
+            return v1.Equals(other.v1) && v2.Equals(other.v2);
+        }
+
+        return false;
     }
 }
 
-class UnionFind {
+public class DisjointSet {
+    public int n;
     public int[] parent;
 
-    public UnionFind(int size) {
-        parent = new int[size];
-        for(int i = 0; i < size; i++) {
+    public DisjointSet(int x) {
+        n = x;
+        parent = new int[n];
+        for(int i = 0; i < n; i++) {
             parent[i] = i;
         }
     }
 
-    public int Find(int node) {
-        if(node != parent[node]) {
-            parent[node] = Find(parent[node]);
+    public int Find(int x) {
+        if(parent[x] != x) {
+            parent[x] = Find(parent[x]);
         }
 
-        return parent[node];
+        return parent[x];
     }
 
-    public bool Union(int n1, int n2) {
-        int root1 = Find(n1);
-        int root2 = Find(n2);
+    public void Union(int x, int y) {
+        var rootX = Find(x);
+        var rootY = Find(y);
 
-        if(root1 == root2) {
-            return false;
+        if(rootX != rootY) {
+            parent[rootX] = rootY;
         }
+    }
 
-        parent[root1] = root2;
-        return true;
+    public bool AreConnected(int x, int y) {
+        return Find(x) == Find(y);
     }
 }
