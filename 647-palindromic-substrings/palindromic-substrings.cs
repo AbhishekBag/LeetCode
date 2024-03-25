e0148ecf93a673b7573a465d98f33c9c37d77762
Abhishek <48788855+AbhishekBag@users.noreply.github.com>
2024-03-25 19:44:25 +0530

    Time: 46 ms (76.85%) | Memory: 37.3 MB (92.22%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cs
+++ 647-palindromic-substrings/palindromic-substrings.cs
@@ -4,16 +4,15 @@ public class Solution {
         int n = s.Length;
 
         for(int i = 0; i < n; i++) {
-            count += CountPalindromicSubstrings(s, i, i);
-            count += CountPalindromicSubstrings(s, i, i + 1);            
+            count += CountPalindromicSubstrings(s, i, i, n);
+            count += CountPalindromicSubstrings(s, i, i + 1, n);            
         }
 
         return count++;
     }
 
-    private int CountPalindromicSubstrings(string s, int l, int r) {
+    private int CountPalindromicSubstrings(string s, int l, int r, int n) {
         int count = 0;
-        int n = s.Length;
         while(l >= 0 && r < n && s[l] == s[r]) {
             count++;
             l--;
