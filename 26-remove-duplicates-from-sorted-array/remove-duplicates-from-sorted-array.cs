23d6d81170db60751d96a522f818cf06c689a2ba
Abhishek <48788855+AbhishekBag@users.noreply.github.com>
2024-04-21 21:08:45 +0530

    Time: 121 ms (57.61%) | Memory: 49.9 MB (76.96%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cs
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cs
@@ -1,17 +1,19 @@
 public class Solution {
     public int RemoveDuplicates(int[] nums) {
         int n = nums.Length;
-        if (n <= 1) {
-            return n;
-        }
+        // if(n <= 1) {
+        // //     return n;
+        // // }
 
         int i = 0;
-        for (int j = 1; j < n; j++) {
-            if (nums[i] != nums[j]) {
-                if (j - i > 1) {
-                    nums[i + 1] = nums[j];
-                }
+        for(int j = 1; j < n; j++) {
+            // if(nums[i] == nums[j]) {
+            //     continue;
+            // }
+
+            if(nums[i] != nums[j]) {
                 i++;
+                nums[i] = nums[j];
             }
         }
 
