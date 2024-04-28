8dded3ead89c18f06c6c6296e3e26ab02f9b7d2a
Abhishek <48788855+AbhishekBag@users.noreply.github.com>
2024-04-28 19:28:35 +0530

    Time: 65 ms (39.90%) | Memory: 42.9 MB (41.94%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cs
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cs
@@ -4,7 +4,7 @@ public class Solution {
         if(n <= 1) {
             return n;
         }
-        
+
         Dictionary<char, int> map = new Dictionary<char, int>();
         int i = 0, maxL = 1;
         map.Add(s[i], 1);
@@ -13,7 +13,6 @@ public class Solution {
             if(map.ContainsKey(c)) {
                 map.Remove(s[i]);
                 i++;
-                continue;
             } else {
                 map[c] = 1;
                 maxL = Math.Max(maxL, j - i + 1);
