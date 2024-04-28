8b92a7635e671815c628ef5f1992760a77e6fe5b
Abhishek <48788855+AbhishekBag@users.noreply.github.com>
2024-04-28 19:27:46 +0530

    Time: 55 ms (72.51%) | Memory: 42.8 MB (48.80%) - LeetSync

---
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cs
@@ -0,0 +1,26 @@
+public class Solution {
+    public int LengthOfLongestSubstring(string s) {
+        int n = s.Length;
+        if(n <= 1) {
+            return n;
+        }
+        
+        Dictionary<char, int> map = new Dictionary<char, int>();
+        int i = 0, maxL = 1;
+        map.Add(s[i], 1);
+        for(int j = i + 1; j < n;) {
+            char c = s[j];
+            if(map.ContainsKey(c)) {
+                map.Remove(s[i]);
+                i++;
+                continue;
+            } else {
+                map[c] = 1;
+                maxL = Math.Max(maxL, j - i + 1);
+                j++;
+            }
+        }
+
+        return maxL;
+    }
+}
