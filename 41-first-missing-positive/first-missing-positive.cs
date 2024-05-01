38983adef310d854dae5e7a751a7164e50ac376c
Abhishek <48788855+AbhishekBag@users.noreply.github.com>
2024-05-01 16:39:04 +0530

    Time: 219 ms (20.01%) | Memory: 64.8 MB (6.36%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.cs
+++ 41-first-missing-positive/first-missing-positive.cs
@@ -9,15 +9,12 @@ public class Solution {
         int i = 0;
         for(; i < nums.Length; i++) {
             if(nums[i] > 0 && nums[i] < 100001) {
-                // Console.WriteLine($"Processing i: {i}, nums[i]: {nums[i]}");
                 arr[nums[i]] = nums[i];
             }                
         }
 
         for(i = 1; i <= 100000; i++) {
-            // Console.WriteLine($"i: {i}, arr[i]: {arr[i]}");
             if(arr[i] == 0) {
-                // Console.WriteLine($"Returning i: {i}");
                 return i;
             }
         }
