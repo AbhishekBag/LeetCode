6550e97404d13f552c6e37f6ab44c0fcc278ab29
Abhishek <48788855+AbhishekBag@users.noreply.github.com>
2024-05-01 16:38:30 +0530

    Time: 355 ms (9.77%) | Memory: 103.9 MB (5.10%) - LeetSync

---
+++ 41-first-missing-positive/first-missing-positive.cs
@@ -0,0 +1,27 @@
+public class Solution {
+    public int FirstMissingPositive(int[] nums) {
+        if(nums.Length == 1 && nums[0] == 1) {
+            return 2;
+        }
+
+        int[] arr = new int[100001];
+
+        int i = 0;
+        for(; i < nums.Length; i++) {
+            if(nums[i] > 0 && nums[i] < 100001) {
+                Console.WriteLine($"Processing i: {i}, nums[i]: {nums[i]}");
+                arr[nums[i]] = nums[i];
+            }                
+        }
+
+        for(i = 1; i <= 100000; i++) {
+            // Console.WriteLine($"i: {i}, arr[i]: {arr[i]}");
+            if(arr[i] == 0) {
+                // Console.WriteLine($"Returning i: {i}");
+                return i;
+            }
+        }
+
+        return nums.Length + 1;
+    }
+}
