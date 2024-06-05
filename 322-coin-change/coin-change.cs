138d253937c425877fd058d04c3595f233fd3b93
Abhishek <48788855+AbhishekBag@users.noreply.github.com>
2024-06-06 00:14:12 +0530

    Time: 119 ms (27.17%) | Memory: 66.1 MB (6.74%) - LeetSync

--- 322-coin-change/coin-change.cs
+++ 322-coin-change/coin-change.cs
@@ -1,45 +1,38 @@
 public class Solution {
-    int[][] mem;
+    private int[][] mem;
     public int CoinChange(int[] coins, int amount) {
-        // mem = Enumerable.Repeat(-1, amount + 1).ToArray().Repeat(coins.Length);
-
         mem = new int[coins.Length][];
         for(int i = 0; i < coins.Length; i++) {
             mem[i] = Enumerable.Repeat(-1, amount + 1).ToArray();
-        }        
+        }
 
-        int res = FindMinComb(coins, amount, 0);
+        int count = GetCoinCount(coins, amount, 0);
 
-        if(res >= 99999999) {
+        if(count >= 99999999) {
             return -1;
         }
 
-        return res;
+        return count;
     }
 
-    private int FindMinComb(int[] coins, int amount, int current) {
+    private int GetCoinCount(int[] coins, int amount, int index) {
+        if(amount < 0 || index >= coins.Length) {
+            return 99999999;
+        }
+
         if(amount == 0) {
             return 0;
         }
 
-        if(current >= coins.Length || amount < 0) {
-            return 99999999;
+        if(mem[index][amount] != -1) {
+            return mem[index][amount];
         }
 
-        if(mem[current][amount] != -1) {
-            return mem[current][amount];
-        }
+        int count1 = 1 + GetCoinCount(coins, amount - coins[index], index);
+        int count2 = GetCoinCount(coins, amount, index + 1);
 
-        int op1 = 1 + FindMinComb(coins, amount - coins[current], current);
-        int op2 = FindMinComb(coins, amount, current + 1);
+        mem[index][amount] = Math.Min(count1, count2);
 
-        mem[current][amount] = Math.Min(op1, op2);
-
-        return mem[current][amount];
+        return mem[index][amount];
     }
 }
-
-/*
-1, 2, 5    11
-f() => 1 + f(11 - 1)   ,  f(11, next index)
-*/
