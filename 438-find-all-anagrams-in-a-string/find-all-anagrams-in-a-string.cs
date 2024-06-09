02b88e0958e58235fe885824290ed0c0863e555e
Abhishek <48788855+AbhishekBag@users.noreply.github.com>
2024-06-09 19:39:50 +0530

    Time: 167 ms (23.83%) | Memory: 50.1 MB (71.81%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cs
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cs
@@ -1,42 +1,46 @@
 public class Solution {
-    public IList<int> FindAnagrams(string s, string p) {
+    public IList<int> FindAnagrams(string s, string p) {        
         Dictionary<char, int> pMap = new Dictionary<char, int>();
         Dictionary<char, int> sMap = new Dictionary<char, int>();
         List<int> res = new List<int>();
 
-        pMap = GetCharCount(p);
-        int j = 0, i = 0;
-        for(; j < s.Length; j++) {
-            char c = s[j];
+        if(s.Length < p.Length) {
+            return res;
+        }
+        
+        pMap = FindCharMap(p, p.Length);
+        sMap = FindCharMap(s, p.Length);
 
+        if(IsAnagram(pMap, sMap)) {
+            res.Add(0);
+        }
+
+        for(int i = p.Length; i < s.Length; i++) {
+            char c = s[i];
             if(sMap.ContainsKey(c)) {
                 sMap[c]++;
             } else {
                 sMap[c] = 1;
             }
 
-            if(j > p.Length - 1) {
-                char iC = s[i++];
-                if(sMap.ContainsKey(iC) && sMap[iC] == 1) {
-                    sMap.Remove(iC);
-                } else {
-                    sMap[iC]--;
-                }
+            char cPrev = s[i - p.Length];
+            if(sMap.ContainsKey(cPrev) && sMap[cPrev] == 1) {
+                sMap.Remove(cPrev);
+            } else {
+                sMap[cPrev]--;
             }
 
-            if(j >= p.Length - 1) {
-                if(IsAnagram(pMap, sMap)) {
-                    res.Add(i);
-                }
+            if(IsAnagram(pMap, sMap)) {
+                res.Add(i - p.Length + 1);
             }
         }
 
         return res;
     }
 
-    public bool IsAnagram(Dictionary<char, int> pMap, Dictionary<char, int> sMap) {
+    private bool IsAnagram(Dictionary<char, int> pMap, Dictionary<char, int> sMap) {
         foreach(var item in pMap) {
-            if(!sMap.ContainsKey(item.Key) || item.Value != sMap[item.Key]) {
+            if(!sMap.ContainsKey(item.Key) || sMap[item.Key] != item.Value) {
                 return false;
             }
         }
@@ -44,16 +48,17 @@ public class Solution {
         return true;
     }
 
-    public Dictionary<char, int> GetCharCount(string str) {
-        Dictionary<char, int> pMap = new Dictionary<char, int>();
-        foreach(char c in str) {
-            if(pMap.ContainsKey(c)) {
-                pMap[c]++;
+    private Dictionary<char, int> FindCharMap(string str, int l) {
+        Dictionary<char, int> cMap = new Dictionary<char, int>();
+        for(int i = 0; i < l; i++) {
+            char c = str[i];
+            if(cMap.ContainsKey(c)) {
+                cMap[c]++;
             } else {
-                pMap[c] = 1;
+                cMap[c] = 1;
             }
         }
 
-        return pMap;
+        return cMap;
     }
 }
