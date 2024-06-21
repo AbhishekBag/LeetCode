98c127f5f5d8e87d1acd4c25f3b074f1f6a6d053
Abhishek <48788855+AbhishekBag@users.noreply.github.com>
2024-06-21 16:48:52 +0530

    Time: 112 ms (99.06%) | Memory: 50.1 MB (92.30%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cs
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cs
@@ -1,26 +1,26 @@
 public class Solution {
-    public int[] TopKFrequent(int[] nums, int k) {        
-        var map = new Dictionary<int, int>();
+    public int[] TopKFrequent(int[] nums, int k) {
         List<int> res = new List<int>();
+        Dictionary<int, int> map = new Dictionary<int, int>();
 
         foreach(var num in nums) {
             if(!map.ContainsKey(num)) {
-                map[num] = 0;
+                map[num] = 1;
+            } else {
+                map[num]++;
             }
-
-            map[num]++;
         }
 
-        map = map.OrderByDescending(e => e.Value)
-                .ToDictionary(i => i.Key, i => i.Value);
-
+        PriorityQueue<int, int> pQueue = new PriorityQueue<int, int>();
         foreach(var item in map) {
-            if(k == 0) {
-                break;
+            pQueue.Enqueue(item.Key, item.Value);
+            if(pQueue.Count > k) {
+                pQueue.Dequeue();
             }
+        }
 
-            k--;
-            res.Add(item.Key);
+        while(pQueue.Count > 0) {
+            res.Add(pQueue.Dequeue());
         }
 
         return res.ToArray();
