21cd09f7494f7578529ab429f839d4ea2286878c
Abhishek <48788855+AbhishekBag@users.noreply.github.com>
2024-07-06 22:15:30 +0530

    Time: 133 ms (5.95%) | Memory: 47.7 MB (18.28%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cs
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cs
@@ -14,54 +14,40 @@
 public class Solution {
     public IList<IList<int>> LevelOrder(TreeNode root) {
         List<IList<int>> res = new List<IList<int>>();
+        Queue<QNode> queue = new Queue<QNode>();
+
         if(root == null) {
             return res;
         }
 
-        Queue<Node> q = new Queue<Node>();
-        q.Enqueue(new Node(root, 0));
+        queue.Enqueue(new QNode(root, 0));
 
-        var prev = q.Dequeue();
-        res.Add(new List<int>() {prev.node.val});
-
-        if(prev.node.left != null) {
-            q.Enqueue(new Node(prev.node.left, prev.level + 1));
-        }
-
-        if(prev.node.right != null) {
-            q.Enqueue(new Node(prev.node.right, prev.level + 1));
-        }
-
-        while(q.Count() > 0) {
-            var poped = q.Dequeue();
-            var lst = res.LastOrDefault();
-
-            if(poped.level == prev.level) {
-                res.LastOrDefault().Add(poped.node.val);
+        while(queue.Count > 0) {
+            var poped = queue.Dequeue();
+            if(res.Count == 0 || res.Count == poped.level) {
+                res.Add(new List<int>() { poped.node.val });
             } else {
-                res.Add(new List<int>() {poped.node.val});
+                res.LastOrDefault().Add(poped.node.val);
             }
 
             if(poped.node.left != null) {
-                q.Enqueue(new Node(poped.node.left, poped.level + 1));
+                queue.Enqueue(new QNode(poped.node.left, poped.level + 1));
             }
 
             if(poped.node.right != null) {
-                q.Enqueue(new Node(poped.node.right, poped.level + 1));
+                queue.Enqueue(new QNode(poped.node.right, poped.level + 1));
             }
-
-            prev = poped;
         }
 
         return res;
     }
 }
 
-public class Node {
+public class QNode {
     public TreeNode node;
     public int level;
 
-    public Node(TreeNode n, int l) {
+    public QNode(TreeNode n, int l) {
         node = n;
         level = l;
     }
