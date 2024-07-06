308732bb429fb0c0c1f59b49ceb481e98d5ee952
Abhishek <48788855+AbhishekBag@users.noreply.github.com>
2024-07-06 22:30:33 +0530

    Time: 52 ms (93.91%) | Memory: 44.6 MB (42.89%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cs
+++ 125-valid-palindrome/valid-palindrome.cs
@@ -1,14 +1,14 @@
 public class Solution {
     public bool IsPalindrome(string s) {
-        s = CleanString(s.ToLower());
-        int n = s.Length;
-        
-        if(n <= 0) {
+        string str = CleanString(s.ToLower());
+        int n = str.Length;
+
+        if(n == 0) {
             return true;
         }
 
-        for(int i = 0; i < n / 2; i++) {
-            if(s[i] != s[n - i - 1]) {
+        for(int i = 0; i < n/2; i++) {
+            if(str[i] != str[n - i - 1]) {
                 return false;
             }
         }
@@ -19,7 +19,7 @@ public class Solution {
     private string CleanString(string s) {
         StringBuilder sb = new StringBuilder();
         foreach(char c in s) {
-            if(Char.IsLetterOrDigit(c)) {// c >= 'a' && c <= 'z' || c >= '0' && c <= '9') {
+            if(Char.IsLetterOrDigit(c)) {
                 sb.Append(c);
             }
         }
