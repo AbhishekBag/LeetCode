78b87f81341bebb9460db18085d5c53cf0064744
Abhishek <48788855+AbhishekBag@users.noreply.github.com>
2024-07-07 20:41:13 +0530

    Time: 164 ms (16.30%) | Memory: 42.5 MB (24.07%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cs
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cs
@@ -1,22 +1,5 @@
 public class Solution {
     public int LengthOfLIS(int[] nums) {
-        return GetLIS(nums);
-        /*
-        int[] map = Enumerable.Repeat(1, nums.Length).ToArray();
-
-        for(int i = 1; i < nums.Length; i++) {
-            if(nums[i] > nums[i - 1]) {
-                map[i] = map[i - 1] + 1;
-            } else {
-                map[i] = map[i - 1];
-            }
-        }
-
-        return map[nums.Length - 1];
-        */
-    }
-
-    private int GetLIS(int[] nums) {
         int[] dp = Enumerable.Repeat(1, nums.Length).ToArray();
         int res = 1;
 
