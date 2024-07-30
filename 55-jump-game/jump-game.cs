deeaebe2b853e4713af7f5f19f4f909a5751dba8
Abhishek <48788855+AbhishekBag@users.noreply.github.com>
2024-07-30 22:08:38 +0530

    Time: 201 ms (78.57%) | Memory: 61.7 MB (70.55%) - LeetSync

--- 55-jump-game/jump-game.cs
+++ 55-jump-game/jump-game.cs
@@ -1,35 +1,20 @@
 public class Solution {
     public bool CanJump(int[] nums) {
-        // HashSet<int> possible = new HashSet<int>();
         int n = nums.Length;
         if(n == 1) {
             return true;
         }
 
-        // possible.Add(n-1);
         int lastAdded = n - 1;
 
         for(int i = n - 2; i >= 0; i--) {
-            // for(int j = nums[i]; j > 0; j--) {
-                int jump = i + nums[i];
-                // if(possible.Contains(jump) || jump >= n) {
-                //     possible.Add(i);
-                //     lastAdded = i;
-                //     break;
-                // }
+            int jump = i + nums[i];
 
-                if(jump >= lastAdded) {
-                    lastAdded = i;
-                    // break;
-                }
-
-                // if(jump < lastAdded) {
-                //     break;
-                // }
-            // }
+            if(jump >= lastAdded) {
+                lastAdded = i;
+            }
         }
 
-        // return possible.Contains(0);
         return lastAdded == 0;
     }
 }
