2fd7ad40c5924bb2ad7059073d36044451fb58b9
Abhishek <48788855+AbhishekBag@users.noreply.github.com>
2024-09-02 11:41:26 +0530

    Time: 105 ms (35.23%) | Memory: 66.1 MB (5.89%) - LeetSync

--- 322-coin-change/coin-change.cs
+++ 322-coin-change/coin-change.cs
@@ -1,38 +1,36 @@
 public class Solution {
-    private int[][] mem;
+    private int[][] arr;
     public int CoinChange(int[] coins, int amount) {
-        mem = new int[coins.Length][];
+        arr = new int[coins.Length][];
         for(int i = 0; i < coins.Length; i++) {
-            mem[i] = Enumerable.Repeat(-1, amount + 1).ToArray();
+            arr[i] = Enumerable.Repeat(Int32.MaxValue, amount + 1).ToArray();
         }
+        
+        var res = PickCoin(coins, amount, coins.Length - 1);
 
-        int count = GetCoinCount(coins, amount, 0);
-
-        if(count >= 99999999) {
-            return -1;
-        }
-
-        return count;
+        return res >= 99999 ? -1 : res;
     }
 
-    private int GetCoinCount(int[] coins, int amount, int index) {
-        if(amount < 0 || index >= coins.Length) {
-            return 99999999;
+    private int PickCoin(int[] coins, int amount, int index) {
+        if(amount < 0 || index < 0) {
+            return 99999;
         }
 
         if(amount == 0) {
             return 0;
         }
 
-        if(mem[index][amount] != -1) {
-            return mem[index][amount];
+        if(arr[index][amount] != Int32.MaxValue) {
+            return arr[index][amount];
         }
 
-        int count1 = 1 + GetCoinCount(coins, amount - coins[index], index);
-        int count2 = GetCoinCount(coins, amount, index + 1);
+        int picked = PickCoin(coins, amount - coins[index], index);
+        int notPicked = PickCoin(coins, amount, index - 1);
 
-        mem[index][amount] = Math.Min(count1, count2);
+        int path = Math.Min(picked + 1, notPicked);
 
-        return mem[index][amount];
+        arr[index][amount] = path;
+
+        return path;
     }
 }
