d83f78388682921e64b1670aec49a46806eee479
Abhishek <48788855+AbhishekBag@users.noreply.github.com>
2024-09-02 11:58:52 +0530

    Time: 106 ms (35.03%) | Memory: 65.4 MB (6.48%) - LeetSync

--- 322-coin-change/coin-change.cs
+++ 322-coin-change/coin-change.cs
@@ -3,31 +3,32 @@ public class Solution {
     public int CoinChange(int[] coins, int amount) {
         arr = new int[coins.Length][];
         for(int i = 0; i < coins.Length; i++) {
-            arr[i] = Enumerable.Repeat(Int32.MaxValue, amount + 1).ToArray();
+            arr[i] = Enumerable.Repeat(Int32.MinValue, amount + 1).ToArray();
         }
         
         var res = PickCoin(coins, amount, coins.Length - 1);
 
-        return res >= 99999 ? -1 : res;
+        return res >= Int32.MaxValue ? -1 : res;
     }
 
     private int PickCoin(int[] coins, int amount, int index) {
         if(amount < 0 || index < 0) {
-            return 99999;
+            return Int32.MaxValue;
         }
 
         if(amount == 0) {
             return 0;
         }
 
-        if(arr[index][amount] != Int32.MaxValue) {
+        if(arr[index][amount] != Int32.MinValue) {
             return arr[index][amount];
         }
 
-        int picked = PickCoin(coins, amount - coins[index], index);
+        int pk = PickCoin(coins, amount - coins[index], index);
+        int picked = pk == Int32.MaxValue ? Int32.MaxValue : pk + 1;
         int notPicked = PickCoin(coins, amount, index - 1);
 
-        int path = Math.Min(picked + 1, notPicked);
+        int path = Math.Min(picked, notPicked);
 
         arr[index][amount] = path;
 
