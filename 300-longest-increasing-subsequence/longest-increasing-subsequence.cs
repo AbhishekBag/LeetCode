afba61a76cb5506e7899d00684749592497f8f05
Abhishek <48788855+AbhishekBag@users.noreply.github.com>
2024-09-04 00:15:14 +0530

    Time: 351 ms (13.44%) | Memory: 43.7 MB (14.89%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cs
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cs
@@ -1,22 +1,33 @@
 public class Solution {
     public int LengthOfLIS(int[] nums) {
-        int[] dp = Enumerable.Repeat(1, nums.Length).ToArray();
-        int res = 1;
+        Dictionary<int, int> map = new Dictionary<int, int>();
 
-        for(int i = nums.Length - 1; i >= 0; i--) {
-            for(int j = i + 1; j < nums.Length; j++) {
-                if(nums[i] < nums[j]) {
-                    dp[i] = Math.Max(dp[i], dp[j] + 1);
-                    res = Math.Max(res, dp[i]);
-                }
+        map[nums.Length - 1] = 1;
+        for(int i = nums.Length - 2; i >= 0; i--) {
+            var val = FindMaxMatchingValue(nums, map, nums[i]);
+            if(val == Int32.MinValue) {
+                map[i] = 1;
+            } else {
+                map[i] = val + 1;
             }
         }
 
-        return res;
+        int max = 0;
+        foreach(var val in map.Values) {
+            max = Math.Max(max, val);
+        }
+
+        return max;
     }
 
-    /*
-    Each item individually give LIS of 1.
-    For each item i, each later items j where i < j, if nums[i] < num[j], has an option to be considered in LIS, i.e. we can chose jth item or not chose it.
-    */
+    private int FindMaxMatchingValue(int[] nums, Dictionary<int, int> map, int num) {
+        int max = Int32.MinValue;
+        foreach(var item in map) {
+            if(num < nums[item.Key]) {
+                max = Math.Max(max, item.Value);
+            }
+        }
+
+        return max;
+    }
 }
