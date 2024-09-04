492401bd2cb650fa69e20a4b6fcfc4b316cd1f90
Abhishek <48788855+AbhishekBag@users.noreply.github.com>
2024-09-04 23:55:28 +0530

    Time: 82 ms (22.07%) | Memory: 50.3 MB (15.36%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cs
+++ 1250-longest-common-subsequence/longest-common-subsequence.cs
@@ -1,32 +1,45 @@
 public class Solution {
-    int[][] arr;
+    private int[][] arr;
     public int LongestCommonSubsequence(string text1, string text2) {
         arr = new int[text1.Length][];
         for(int i = 0; i < text1.Length; i++) {
             arr[i] = Enumerable.Repeat(-1, text2.Length).ToArray();
         }
+        
+        return GetMaxSubsequenceLength(text1, text2, 0, 0);
 
-        return GetSequenceLength(text1, text2, 0, 0);
+        // return arr[text1.Length - 1][text2.Length - 1];
     }
 
-    private int GetSequenceLength(string text1, string text2, int p1, int p2) {
-        if(p1 >= text1.Length || p2 >= text2.Length) {
+    private int GetMaxSubsequenceLength(string text1, string text2, int i, int j) {
+        if(i >= text1.Length || j >= text2.Length) {
             return 0;
         }
 
-        if(arr[p1][p2] != -1) {
-            return arr[p1][p2];
+        if(arr[i][j] != -1) {
+            return arr[i][j];
         }
 
-        if(text1[p1] != text2[p2]) {
-            int IncreaseT1 = GetSequenceLength(text1, text2, p1 + 1, p2);
-            int IncreaseT2 = GetSequenceLength(text1, text2, p1, p2 + 1);
-
-            arr[p1][p2] = Math.Max(IncreaseT1, IncreaseT2);
+        if(text1[i] == text2[j]) {
+            arr[i][j] = 1 + GetMaxSubsequenceLength(text1, text2, i + 1, j + 1);
         } else {
-            arr[p1][p2] = 1 + GetSequenceLength(text1, text2, p1 + 1, p2 + 1);
-        }
+            arr[i][j] = Math.Max(GetMaxSubsequenceLength(text1, text2, i + 1, j),
+        GetMaxSubsequenceLength(text1, text2, i, j + 1));
+        }        
 
-        return arr[p1][p2];
+        return arr[i][j];
     }
 }
+
+/*
+str1 = abcde (i)
+str2 = ace (j)
+
+i > l1 || j > l2
+return 0;
+
+str1[i] == str2[j]
+return 1 + f(str1, str2, i + 1, j + 1)
+
+return Max(f(str1, str2, i + 1,j), f(str1, str2, i, j + 1))
+*/
