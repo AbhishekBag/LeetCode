887148040bff859bcca3f70a9b964a5ed357a960
Abhishek <48788855+AbhishekBag@users.noreply.github.com>
2024-09-06 23:33:04 +0530

    Time: 70 ms (26.05%) | Memory: 50.2 MB (19.54%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cs
+++ 1250-longest-common-subsequence/longest-common-subsequence.cs
@@ -5,22 +5,12 @@ public class Solution {
         for(int i = 0; i < text1.Length; i++) {
             arr[i] = Enumerable.Repeat(-1, text2.Length).ToArray();
         }
-        
-        GetMaxSubsequenceLength(text1, text2, 0, 0);
 
-        // for(int i = 0; i < text1.Length; i++) {
-        //     for(int j = 0; j < text2.Length; j++) {
-        //         Console.Write($"{arr[i][j]}  ");
-        //     }
-
-        //     Console.WriteLine();
-        // }
-
-        return arr[0][0];
+        return FindMaxLength(text1, text2, 0, 0);
     }
 
-    private int GetMaxSubsequenceLength(string text1, string text2, int i, int j) {
-        if(i >= text1.Length || j >= text2.Length) {
+    private int FindMaxLength(string str1, string str2, int i, int j) {
+        if(i >= str1.Length || j >= str2.Length) {
             return 0;
         }
 
@@ -28,26 +18,12 @@ public class Solution {
             return arr[i][j];
         }
 
-        if(text1[i] == text2[j]) {
-            arr[i][j] = 1 + GetMaxSubsequenceLength(text1, text2, i + 1, j + 1);
+        if(str1[i] == str2[j]) {
+            arr[i][j] = 1 + FindMaxLength(str1, str2, i + 1, j + 1);
         } else {
-            arr[i][j] = Math.Max(GetMaxSubsequenceLength(text1, text2, i + 1, j),
-        GetMaxSubsequenceLength(text1, text2, i, j + 1));
-        }        
+            arr[i][j] = Math.Max(FindMaxLength(str1, str2, i + 1, j), FindMaxLength(str1, str2, i, j + 1));
+        }
 
         return arr[i][j];
     }
 }
-
-/*
-str1 = abcde (i)
-str2 = ace (j)
-
-i > l1 || j > l2
-return 0;
-
-str1[i] == str2[j]
-return 1 + f(str1, str2, i + 1, j + 1)
-
-return Max(f(str1, str2, i + 1,j), f(str1, str2, i, j + 1))
-*/
