7dbfdb6fca494c3d32fdc1fbdb11e20a1ddc5296
Abhishek <48788855+AbhishekBag@users.noreply.github.com>
2024-09-10 23:39:48 +0530

    Time: 157 ms (39.22%) | Memory: 55.4 MB (70.63%) - LeetSync

--- 56-merge-intervals/merge-intervals.cs
+++ 56-merge-intervals/merge-intervals.cs
@@ -1,24 +1,30 @@
 public class Solution {
     public int[][] Merge(int[][] intervals) {
-        Array.Sort(intervals, (a, b) => {
-            return a[0].CompareTo(b[0]);
-        });
+        if(intervals.Length == 0) {
+            return intervals;
+        }
 
-        List<int[]> res =  new List<int[]>();
+        List<int[]> res = new List<int[]>();
+        Array.Sort(intervals, (a, b) => a[0].CompareTo(b[0]));
+
         var current = intervals[0];
         for(int i = 1; i < intervals.Length; i++) {
-            var next = intervals[i];
-            if(current[1] >= next[0]) {
-                current[0] = Math.Min(current[0], next[0]);
-                current[1] = Math.Max(current[1], next[1]);
+            var upcoming = intervals[i];
+            if(IsOverlapping(current, upcoming)) {
+                current[0] = Math.Min(current[0], upcoming[0]);
+                current[1] = Math.Max(current[1], upcoming[1]);
             } else {
                 res.Add(current);
-                current = next;
+                current = upcoming;
             }
         }
 
         res.Add(current);
 
         return res.ToArray();
+    }
+
+    private bool IsOverlapping(int[] interval1, int[] interval2) {
+        return Math.Max(interval1[0], interval2[0]) <= Math.Min(interval1[1], interval2[1]);
     }
 }
