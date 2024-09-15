c40d33f1eacf0976dc74f424b2f94ad95fefb265
Abhishek <48788855+AbhishekBag@users.noreply.github.com>
2024-09-15 21:16:59 +0530

    Time: 84 ms (57.39%) | Memory: 54 MB (37.65%) - LeetSync

---
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cs
@@ -0,0 +1,38 @@
+public class Solution {
+    public string LongestPalindrome(string s) {
+        int n = s.Length;
+        if(n <= 1) {
+            return s;
+        }
+
+        var res = s[0].ToString();
+        for(int i = 0; i < n; i++) {
+            var tmpOdd = GetMaxPalindromicStringCenteredAt(s, i, true);
+            var tmpEven = GetMaxPalindromicStringCenteredAt(s, i, false);
+            var tmp = tmpOdd.Length > tmpEven.Length ? tmpOdd : tmpEven;
+
+            // Console.WriteLine($"i: {i}; res: {res}; tmp: {tmp}");
+            if(res.Length < tmp.Length) {
+                res = tmp;
+            }
+        }
+
+        return res;
+    }
+
+    private string GetMaxPalindromicStringCenteredAt(string s, int center, bool checkOddLengthP) {
+        int n = s.Length;
+
+        // Odd length palindrome
+        int left = checkOddLengthP ? center - 1 : center;
+        int right = center + 1;
+        while(left >= 0 && right < n && s[left] == s[right]) {
+            left--;
+            right++;
+        }
+
+        left += 1;
+        right -= 1;
+        return s.Substring(left, right - left + 1);
+    }
+}
