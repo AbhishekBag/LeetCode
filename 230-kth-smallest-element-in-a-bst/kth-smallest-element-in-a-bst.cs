4547f8be51f5fab4c509493a707d86b5a3be5751
Abhishek <48788855+AbhishekBag@users.noreply.github.com>
2024-09-27 21:44:01 +0530

    Time: 91 ms (11.97%) | Memory: 45.8 MB (28.15%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.cs
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.cs
@@ -12,74 +12,22 @@
  * }
  */
 public class Solution {
+    private int kMin = 0;
+    private List<int> res;
     public int KthSmallest(TreeNode root, int k) {
-        // List<int> items = new List<int>();
-        Stack<TreeNode> collection = new Stack<TreeNode>();
-        // HashSet<TreeNode> visited = new HashSet<TreeNode>();
+        res = new List<int>();
+        FindKSmall(root);
 
-        var cur = root;
-        int n = 0;
-        while(n < k) {
-            while(cur != null) {
-                collection.Push(cur);
-                cur = cur.left;
-            }
-
-            cur = collection.Pop();
-            n++;
-            if(n == k) {
-                return cur.val;
-            }
-
-            cur = cur.right;
-        }
-
-        return -1;
-
-        /*
-        if(root != null) {
-            collection.Push(root);
-            visited.Add(root);
-        }
-
-        while(collection.Count > 0 || items.Count < k) {
-            var current = collection.Pop();
-
-            // go to left child
-            if(current.left != null && !visited.Contains(current.left)) {
-            collection.Push(current);
-            collection.Push(current.left);
-            visited.Add(current.left);
-
-            continue;
-            }
-
-            // process current node
-            items.Add(current.val);
-
-            // go to right child
-            if(current.right != null) {
-                collection.Push(current.right);
-            }
-        }
-
-        return items[k - 1];
-
-        */
-        /*
-        VisitBST(root, items, k);
-
-        return items[k - 1];
-        */
+        return res[k - 1];
     }
 
-    private void VisitBST(TreeNode root, List<int> items, int k) {
-        if(root == null || items.Count == k) {
+    private void FindKSmall(TreeNode node) {
+        if(node == null) {
             return;
         }
 
-        VisitBST(root.left, items, k);
-        items.Add(root.val);
-        VisitBST(root.right, items, k);
+        FindKSmall(node.left);
+        res.Add(node.val);
+        FindKSmall(node.right);
     }
 }
