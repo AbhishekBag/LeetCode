a8c208667ce24a2172c284fd15f469e153c1cd74
Abhishek <48788855+AbhishekBag@users.noreply.github.com>
2024-09-27 21:25:56 +0530

    Time: 88 ms (28.07%) | Memory: 45.7 MB (27.31%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.cs
+++ 98-validate-binary-search-tree/validate-binary-search-tree.cs
@@ -13,22 +13,22 @@
  */
 public class Solution {
     public bool IsValidBST(TreeNode root) {
-        return ValidBST(root, null, null);
+        return ValidateBST(root, null, null);
     }
 
-    private bool ValidBST(TreeNode node, TreeNode min, TreeNode max) {
+    private bool ValidateBST(TreeNode node, TreeNode min, TreeNode max) {
         if(node == null) {
             return true;
         }
 
-        if(min != null && node.val >= min.val) {
+        if(min != null && node.val <= min.val) {
             return false;
         }
 
-        if(max != null && node.val <= max.val) {
+        if(max != null && node.val >= max.val) {
             return false;
         }
 
-        return ValidBST(node.left, node, max) && ValidBST(node.right, min, node);
+        return ValidateBST(node.left, min, node) && ValidateBST(node.right, node, max);
     }
 }
