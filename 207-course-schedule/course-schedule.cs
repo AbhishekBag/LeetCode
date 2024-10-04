64de008814fe86c9ae58b4613efe2b52cc5de3e2
Abhishek <48788855+AbhishekBag@users.noreply.github.com>
2024-10-04 18:53:36 +0530

    Time: 114 ms (27.83%) | Memory: 51.2 MB (14.91%) - LeetSync

--- 207-course-schedule/course-schedule.cs
+++ 207-course-schedule/course-schedule.cs
@@ -1,50 +1,52 @@
 public class Solution {
     public bool CanFinish(int numCourses, int[][] prerequisites) {
-        Dictionary<int, int> indeg = new Dictionary<int, int>();
-        Dictionary<int, List<int>> outdeg = new Dictionary<int, List<int>>();
+        Dictionary<int, int> inDeg = new Dictionary<int, int>();
+        Dictionary<int, List<int>> outDeg = new Dictionary<int, List<int>>();
         HashSet<int> visited = new HashSet<int>();
         Queue<int> available = new Queue<int>();
 
         foreach(var item in prerequisites) {
-            int i = item[0];
-            int j = item[1];
-
-            if(indeg.ContainsKey(i)) {
-                indeg[i]++;
-            } else {
-                indeg[i] = 1;
+            int item1 = item[0];
+            int item2 = item[1];
+            if(!inDeg.ContainsKey(item1)) {
+                inDeg[item1] = 0;
             }
 
-            if(outdeg.ContainsKey(j)) {
-                outdeg[j].Add(i);
-            } else {
-                outdeg[j] = new List<int> () { i };
+            inDeg[item1]++;
+
+            if(!outDeg.ContainsKey(item2)) {
+                outDeg[item2] = new List<int>();
+            }
+
+            outDeg[item2].Add(item1);
+        }
+
+        for(int i = 0; i < numCourses; i++) {
+            if(!inDeg.ContainsKey(i)) {
+                available.Enqueue(i);
             }
         }
 
-        for(int course = 0; course < numCourses; course++) {
-            if(!indeg.ContainsKey(course)) {
-                available.Enqueue(course);
-            }
-        }
-
-        while(available.Count() > 0) {
-            var course = available.Dequeue();
-            if(!visited.Contains(course)) {
-                visited.Add(course);
-                if(outdeg.ContainsKey(course)) {
-                    foreach(var item in outdeg[course]) {
-                        if(indeg.ContainsKey(item)) {
-                            indeg[item]--;
-                            if(indeg[item] == 0) {
+        while(available.Count > 0) {
+            var cur = available.Dequeue();
+            if(!visited.Contains(cur)) {
+                visited.Add(cur);
+                if(outDeg.ContainsKey(cur)) {
+                    foreach(var item in outDeg[cur]) {
+                        if(inDeg.ContainsKey(item)) {
+                            if(inDeg[item] == 1) {
+                                inDeg.Remove(item);
                                 available.Enqueue(item);
+                            } else {
+                                inDeg[item]--;
                             }
                         }
                     }
                 }
+                
             }
         }
 
-        return visited.Count() == numCourses ? true : false;
+        return visited.Count == numCourses;
     }
 }
