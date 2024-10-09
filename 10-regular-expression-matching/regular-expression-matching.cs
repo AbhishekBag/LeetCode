f602dbf81b78c3cd96e97add96c17d3646c469ec
Abhishek <48788855+AbhishekBag@users.noreply.github.com>
2024-10-10 00:19:06 +0530

    Time: 51 ms (92.67%) | Memory: 42.6 MB (29.33%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.cs
+++ 10-regular-expression-matching/regular-expression-matching.cs
@@ -1,9 +1,22 @@
 public class Solution {
+    private int[][] memo;
+    private Dictionary<(int, int), bool> cache;
     public bool IsMatch(string s, string p) {
+        int n = s.Length;
+        int m = p.Length;
+        cache = new Dictionary<(int, int), bool>();
+        memo = new int[n][];
+        for(int i = 0; i < n; i++) {
+            memo[i] = Enumerable.Repeat(-1, m).ToArray();
+        }
+
         return MatchString(s, p, 0, 0);
     }
 
     private bool MatchString(string s, string p, int i, int j) {
+        if(cache.ContainsKey((i, j))) {
+            return cache[(i, j)];
+        }
         int n = s.Length;
         int m = p.Length;
         if(i >= n && j >= m) {
@@ -14,15 +27,27 @@ public class Solution {
             return false;
         }
 
+        // if(memo[i][j] != -1) {
+        //     return memo[i][j] == 1;
+        // }
+
         bool match = (i < n && (s[i] == p[j] || p[j] == '.'));
         if(j + 1 < m && p[j + 1] == '*') {
-            return MatchString(s, p, i, j + 2) || (match && MatchString(s, p, i + 1, j));
+            cache[(i, j)] = MatchString(s, p, i, j + 2) || (match && MatchString(s, p, i + 1, j));
+            // memo[i][j] = res1 ? 1 : 0;
+            return cache[(i, j)];
         }
 
         if(match) {
-            return MatchString(s, p, i + 1, j + 1);
+            cache[(i, j)] = MatchString(s, p, i + 1, j + 1);
+            // memo[i][j] = res2 ? 1 : 0;
+            return cache[(i, j)];
         }
 
-        return false;
+        // memo[i][j] = 0;
+
+        // return memo[i][j] == 1;
+        cache[(i, j)] = false;
+        return cache[(i, j)];
     }
 }
