1cdc0d79100822e8f86a1cafbce6cd18617eeedb
Abhishek <48788855+AbhishekBag@users.noreply.github.com>
2024-10-14 22:44:11 +0530

    Time: 231 ms (21.40%) | Memory: 61.6 MB (84.72%) - LeetSync

--- 55-jump-game/jump-game.cs
+++ 55-jump-game/jump-game.cs
@@ -1,13 +1,13 @@
 public class Solution {
     public bool CanJump(int[] nums) {
-        int lastGoodPosition = nums.Length - 1;
+        int lastGoodSpot = nums.Length - 1;
 
         for(int i = nums.Length - 2; i >= 0; i--) {
-            if(i + nums[i] >= lastGoodPosition) {
-                lastGoodPosition = i;
+            if(nums[i] + i >= lastGoodSpot) {
+                lastGoodSpot = i;
             }
         }
 
-        return lastGoodPosition == 0;
+        return lastGoodSpot == 0;
     }
 }
