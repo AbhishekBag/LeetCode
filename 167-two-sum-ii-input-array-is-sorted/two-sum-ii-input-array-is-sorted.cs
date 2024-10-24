93bc8a42375f030ffc1b017b6da58fcd9a23e885
Abhishek <48788855+AbhishekBag@users.noreply.github.com>
2024-10-24 10:09:58 +0530

    Time: 3 ms (93.14%) | Memory: 49.8 MB (54.30%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cs
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cs
@@ -1,36 +1,32 @@
 public class Solution {
     public int[] TwoSum(int[] numbers, int target) {
-        int[] res = new int[2];
-        for(int i = 0; i < numbers.Length - 1; i++) {
-            int k = BinarySearch(numbers, i + 1, target - numbers[i]);
-
-            // Console.WriteLine($"i: {i}; target: {target}; searchTarget: {target - numbers[i]}; returnedIndex: {k}");
+        int [] res = new int[2];
+        for(int i = 0; i < numbers. Length; i++) {
+            int k = BinarySearch(numbers, target - numbers[i], i + 1);
 
             if(k != -1) {
                 res[0] = i + 1;
                 res[1] = k + 1;
 
-                break;
+                return res;
             }
         }
 
         return res;
     }
 
-    private int BinarySearch(int[] nums, int s, int target) {
-        int n = nums.Length - 1;
+    private int BinarySearch(int[] numbers, int target, int start) {
+        int n = numbers.Length - 1;
         int mid = 0;
-        while(s <= n) {
-            mid = (s + n) / 2;
-            // Console.WriteLine($"s: {s}; mid: {mid}; n: {n}");
-            if(nums[mid] == target) {
+        while(start <= n) {
+            mid = (start + n) / 2;
+            if(numbers[mid] == target) {
                 return mid;
             }
-
-            if(nums[mid] > target) {
+            if(numbers[mid] > target) {
                 n = mid - 1;
             } else {
-                s = mid + 1;
+                start = mid + 1;
             }
         }
 
