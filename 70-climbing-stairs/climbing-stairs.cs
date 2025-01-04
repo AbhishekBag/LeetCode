1395a74a885e7a0d8b65335ab4d9eedac3efe61f
Abhishek <48788855+AbhishekBag@users.noreply.github.com>
2025-01-04 21:22:48 +0530

    Time: 0 ms (100.00%) | Memory: 27.5 MB (42.13%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cs
+++ 70-climbing-stairs/climbing-stairs.cs
@@ -1,20 +1,18 @@
 public class Solution {
-    private int[] dp;
     public int ClimbStairs(int n) {
-        dp = Enumerable.Repeat(-1, n + 1).ToArray();
-        return Climb(n);
-    }
-
-    private int Climb(int n) {
         if(n <= 2) {
             return n;
         }
 
-        if(dp[n] != -1) {
-            return dp[n];
+        int cur = 0;
+        int prev1 = 2, prev2 = 1;
+
+        for(int i = 3; i <= n; i++) {
+            cur = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = cur;
         }
 
-        dp[n] = Climb(n - 1) + Climb(n - 2);
-        return dp[n];
+        return cur;
     }
 }
