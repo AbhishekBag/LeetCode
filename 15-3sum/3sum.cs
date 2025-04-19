da6ed98d99d051a42d354a1bd2614cdb31d19267
Abhishek <48788855+AbhishekBag@users.noreply.github.com>
2025-04-19 21:03:41 +0530

    Time: 33 ms (84.74%) | Memory: 66 MB (31.20%) - LeetSync

--- 15-3sum/3sum.cs
+++ 15-3sum/3sum.cs
@@ -1,22 +1,22 @@
 public class Solution {
     public IList<IList<int>> ThreeSum(int[] nums) {
-        List<IList<int>> res = new List<IList<int>>();
-        int n = nums.Length;
-
+        List<IList<int>> res = new();
         Array.Sort(nums);
-        for(int i = 0; i < n - 2; i++) {
-            if(i > 0 && nums[i] == nums[i-1]) {
+
+        for(int i = 0; i < nums.Length - 2; i++) {
+            if(i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
 
             int left = i + 1;
-            int right = n - 1;
-            while(left < right){
+            int right = nums.Length - 1;
+            while(left < right) {
                 int sum = nums[i] + nums[left] + nums[right];
                 if(sum == 0) {
                     res.Add(new List<int> { nums[i], nums[left], nums[right]});
-                    while(left < right && nums[left] == nums[left + 1]) { left++;};
-                    while(left < right && nums[right] == nums[right - 1]) { right--;};
+                    while(left < right && nums[left] == nums[left + 1]) left++;
+                    while(left < right && nums[right] == nums[right - 1]) right--;
+
                     left++;
                     right--;
                 } else if(sum < 0) {
@@ -24,17 +24,9 @@ public class Solution {
                 } else {
                     right--;
                 }
-            }            
+            }
         }
-
+        
         return res;
     }
 }
-
-/*
--1,0,1,2,-1,-4
--4,-1,-1,0,1,2
-
-
-
-*/
