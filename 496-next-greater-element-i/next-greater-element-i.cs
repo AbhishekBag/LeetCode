14fe90d82c61e02e2a534138ca4fa0a127f13172
Abhishek <48788855+AbhishekBag@users.noreply.github.com>
2025-05-15 21:04:10 +0530

    Time: 3 ms (46.85%) | Memory: 48.5 MB (6.85%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cs
+++ 496-next-greater-element-i/next-greater-element-i.cs
@@ -2,20 +2,30 @@ public class Solution {
     public int[] NextGreaterElement(int[] nums1, int[] nums2) {
         Stack<int> stk = new ();
         Dictionary<int, int> map = new ();
+        int[] arr = new int[10001];
         int[] res = new int[nums1.Length];
 
         for(int i = 0; i < nums2.Length; i++) {
             while(stk.Count() != 0 && stk.Peek() < nums2[i]) {
                 var poped = stk.Pop();
-                map[poped] = nums2[i];
+                // map[poped] = nums2[i];
+                arr[poped] = nums2[i];
             }
 
             stk.Push(nums2[i]);
         }
 
+        // for(int i = 0; i < nums1.Length; i++) {
+        //     if(map.ContainsKey(nums1[i])) {
+        //         res[i] = map[nums1[i]];
+        //     } else {
+        //         res[i] = -1;
+        //     }
+        // }
+
         for(int i = 0; i < nums1.Length; i++) {
-            if(map.ContainsKey(nums1[i])) {
-                res[i] = map[nums1[i]];
+            if(arr[nums1[i]] != 0) {
+                res[i] = arr[nums1[i]];
             } else {
                 res[i] = -1;
             }
