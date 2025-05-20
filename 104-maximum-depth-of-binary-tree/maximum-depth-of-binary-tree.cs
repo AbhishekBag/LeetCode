ca180a78c333e6195a3df9b770057d2908599d13
Abhishek <48788855+AbhishekBag@users.noreply.github.com>
2025-05-20 23:02:52 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (9.26%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cs
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cs
@@ -16,14 +16,14 @@ public class Solution {
         return GetDepth(root);
     }
 
-    private int GetDepth(TreeNode node) {
-        if (node == null) {
+    private int GetDepth(TreeNode root) {
+        if(root == null) {
             return 0;
         }
 
-        var leftDepth = GetDepth(node.left);
-        var rightDepth = GetDepth(node.right);
+        int left = GetDepth(root.left);
+        int right = GetDepth(root.right);
 
-        return Math.Max(leftDepth, rightDepth) + 1;
+        return Math.Max(left, right) + 1;
     }
 }
