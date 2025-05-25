53ddcb1f0050c34e54575944ccdc5eafe6681423
Abhishek <48788855+AbhishekBag@users.noreply.github.com>
2025-05-25 16:54:25 +0530

    Time: 0 ms (100.00%) | Memory: 47.4 MB (38.86%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.cs
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cs
@@ -11,45 +11,26 @@
  *     }
  * }
  */
- // 1. associate level with each node
- // 2. Use hashset to keep track of visited nodes
- // 3. res => List<int>
- // 4. BFS => insent a node with it's level into a queue
- // 5. pop from queue
- // 6. if the level is present in visited set, ignore else add it to res
- // 7. if not null. then enqueue right and left child on the queue with level => current level + 1
 public class Solution {
+    private List<int> res = new ();
     public IList<int> RightSideView(TreeNode root) {
-        List<int> res = new List<int>();
-
-        if(root == null) {
-            return res;
-        }
-        HashSet<int> visited = new HashSet<int>();
-        Queue<(TreeNode node, int level)> currentSet = new Queue<(TreeNode node, int level)>();
-
-        currentSet.Enqueue((root, 0));
-
-        while(currentSet.Count() > 0) {
-            var current = currentSet.Dequeue();
-            // Console.WriteLine($"Processing: {current.node.val} at level: {current.level}");
-
-            if(!visited.Contains(current.level)) {
-                // Console.WriteLine($"Added {current.node.val}");
-                res.Add(current.node.val);
-                visited.Add(current.level);
-            }
-            
-            if(current.node.right != null) {
-                // Console.WriteLine($"inserting: {current.node.right.val}");
-                currentSet.Enqueue((current.node.right, current.level + 1));
-            }
-            if(current.node.left != null) {
-                // Console.WriteLine($"inserting: {current.node.left.val}");
-                currentSet.Enqueue((current.node.left, current.level + 1));
-            }
-        }
+        GetRightView(root, 0);
 
         return res;
+    }
+
+    private void GetRightView(TreeNode node, int level) {
+        if(node == null) {
+            return;
+        }
+
+        if(res.Count <= level) {
+            res.Add(node.val);
+        } else {
+            res[level] = node.val;
+        }
+
+        GetRightView(node.left, level + 1);
+        GetRightView(node.right, level + 1);
     }
 }
