1bcbe9ba1eca43635e6ac3266ff3020a19b70d11
Abhishek <48788855+AbhishekBag@users.noreply.github.com>
2025-06-08 23:46:17 +0530

    Time: 1 ms (27.79%) | Memory: 51 MB (37.66%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cs
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cs
@@ -2,23 +2,23 @@ public class Solution {
     public int[] TwoSum(int[] numbers, int target) {
         int i = 0, j = numbers.Length - 1;
 
-        while(i < j) {
-            // while(i < numbers.Length && numbers[i] + numbers[j] < target) {
-            //     i++;
-            // }
-
-            // while(j >= 0 && numbers[i] + numbers[j] > target) {
-            //     j--;
-            // }
-
-            var sum = numbers[i] + numbers[j];
-            if(sum < target) {
+        while(i <= j) {
+            while(i < numbers.Length && numbers[i] + numbers[j] < target) {
                 i++;
-            } else if(sum >target) {
+            }
+
+            while(j >= 0 && numbers[i] + numbers[j] > target) {
                 j--;
-            } else break;
+            }
+
+            if(numbers[i] + numbers[j] == target) {
+                return new int[2] { i + 1, j + 1 };
+            }
+
+            // i++;
+            // j--;
         }
 
-        return new int[2] {i+1, j+1};
+        return new int[2];
     }
 }
