c6b73fa06d0d19e02b2c10474b2563cf0ece43c1
Abhishek <48788855+AbhishekBag@users.noreply.github.com>
2025-06-14 23:32:20 +0530

    Time: 2 ms (14.35%) | Memory: 43.4 MB (49.24%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cs
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cs
@@ -1,32 +1,21 @@
 public class Solution {
+    int[] memo;
     public int MinCostClimbingStairs(int[] cost) {
-        int[] arr = Enumerable.Repeat(-1, cost.Length).ToArray();
-        
-        return GetMinCost(cost, arr, cost.Length - 1);
+        memo = Enumerable.Repeat(Int32.MaxValue, cost.Length + 1).ToArray();
+        return Math.Min(FindCost(cost, 0), FindCost(cost, 1));
     }
 
-    private int GetMinCost(int[] cost, int[] arr, int i) {
-        if(i <= 0) {
+    private int FindCost(int[] cost, int index) {
+        if(index >= cost.Length) {
             return 0;
         }
 
-        int p1 = 0, p2 = 0;
-        if(arr[i] != -1) {
-            return arr[i];
+        if(memo[index] != Int32.MaxValue) {
+            return memo[index];
         }
 
-        p1 = cost[i] + GetMinCost(cost, arr, i - 1);
-        p2 = cost[i - 1] + GetMinCost(cost, arr, i - 2);
+        memo[index] = Math.Min((FindCost(cost, index + 1) + cost[index]), FindCost(cost, index + 2) + cost[index]);
 
-        arr[i] = Math.Min(p1, p2);
-        return arr[i];
-    }
-
-    private void DisplayCost(int[] arr) {
-        foreach(var item in arr) {
-            Console.Write($"{item}, ");
-        }
-
-        Console.WriteLine();
+        return memo[index];
     }
 }
