ee542f0dd41e31029f997cacdad9fe47e3bdb31d
Abhishek <48788855+AbhishekBag@users.noreply.github.com>
2025-06-14 23:19:46 +0530

    Time: 0 ms (100.00%) | Memory: 29.5 MB (14.91%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cs
+++ 70-climbing-stairs/climbing-stairs.cs
@@ -1,21 +1,32 @@
 public class Solution {
-    private int[] memo;
     public int ClimbStairs(int n) {
-        memo = Enumerable.Repeat(Int32.MaxValue, n + 1).ToArray();
-        return CountWays(n);
-    }
+        int a = 0, b = 1, c = 1;
 
-    private int CountWays(int n) {
-        if(n <= 2) {
-            return n;
+        for(int i = 1; i <= n; i++) {
+            c = a + b;
+            a = b;
+            b = c;
         }
 
-        if(memo[n] != Int32.MaxValue) {
-            return memo[n];
-        }
-
-        memo[n] = CountWays(n - 1) + CountWays(n - 2);
-
-        return memo[n];
+        return c;
     }
+    // private int[] memo;
+    // public int ClimbStairs(int n) {
+    //     memo = Enumerable.Repeat(Int32.MaxValue, n + 1).ToArray();
+    //     return CountWays(n);
+    // }
+
+    // private int CountWays(int n) {
+    //     if(n <= 2) {
+    //         return n;
+    //     }
+
+    //     if(memo[n] != Int32.MaxValue) {
+    //         return memo[n];
+    //     }
+
+    //     memo[n] = CountWays(n - 1) + CountWays(n - 2);
+
+    //     return memo[n];
+    // }
 }
