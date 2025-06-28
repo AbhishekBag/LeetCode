d81eda6f2c1c364573b3451caf517be8dee31686
Abhishek <48788855+AbhishekBag@users.noreply.github.com>
2025-06-29 01:04:52 +0530

    Time: 4 ms (77.66%) | Memory: 44.5 MB (55.21%) - LeetSync

--- 1483-rank-teams-by-votes/rank-teams-by-votes.cs
+++ 1483-rank-teams-by-votes/rank-teams-by-votes.cs
@@ -1,48 +1,30 @@
 public class Solution {
     public string RankTeams(string[] votes) {
-        if (votes.Length == 1) {
-            return votes[0];
-        }
-
         int n = votes[0].Length;
-        Dictionary<char, int[]> weightedVotes = new Dictionary<char, int[]>();
+        Dictionary<char, int[]> rankMap = new Dictionary<char, int[]>();
 
-        foreach (string vote in votes) {
-            for (int i = 0; i < n; i++) {
-                char c = vote[i];
-                if (!weightedVotes.ContainsKey(c)) {
-                    weightedVotes[c] = new int[n];
-                }
-                weightedVotes[c][i]++;
+        foreach(char c in votes[0]) {
+            rankMap[c] = new int[n];
+        }
+
+        foreach(string vote in votes) {
+            for(int i = 0; i < n; i++) {
+                rankMap[vote[i]][i]++;
             }
         }
 
-        List<char> teams = weightedVotes.Keys.ToList();
-        teams.Sort((a, b) => {
-            for (int i = 0; i < n; i++) {
-                if (weightedVotes[a][i] != weightedVotes[b][i]) {
-                    return weightedVotes[b][i].CompareTo(weightedVotes[a][i]);
+        List<char> rank = rankMap.Keys.ToList();
+
+        rank.Sort((a, b) => {
+            for(int i = 0; i < n; i++) {
+                if(rankMap[a][i] != rankMap[b][i]) {
+                    return rankMap[b][i].CompareTo(rankMap[a][i]);
                 }
             }
-            return a.CompareTo(b); // Compare alphabetically if votes are the same
+
+            return a.CompareTo(b);
         });
 
-        return new string(teams.ToArray());
-    }
-
-    private void DisplayWeightedVotesMap(List<(int, char)> map) {
-        for(int i = 0; i < map.Count(); i++) {
-            Console.Write($"({map[i].Item1} => {map[i].Item2}), ");
-        }
-
-        Console.WriteLine();
-    }
-
-    private void DisplayWeightedVotes(int[] arr) {
-        for(int i = 0; i < arr.Length; i++) {
-            Console.Write($"{arr[i]}, ");
-        }
-
-        Console.WriteLine();
+        return new string(rank.ToArray());
     }
 }
