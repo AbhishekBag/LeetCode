c65f7dbd5b17c2ef9bbd296f40039efffce7920d
Abhishek <48788855+AbhishekBag@users.noreply.github.com>
2025-07-12 00:11:07 +0530

    Time: 23 ms (11.20%) | Memory: 56.5 MB (5.74%) - LeetSync

--- 207-course-schedule/course-schedule.cs
+++ 207-course-schedule/course-schedule.cs
@@ -1,50 +1,51 @@
 public class Solution {
     public bool CanFinish(int numCourses, int[][] prerequisites) {
-        Dictionary<int, int> inDeg = new Dictionary<int, int>();
-        Dictionary<int, List<int>> outDeg = new Dictionary<int, List<int>>();
+        Dictionary<int, HashSet<int>> inboundMap = new Dictionary<int, HashSet<int>>();
+        Dictionary<int, HashSet<int>> outBoundMap = new Dictionary<int, HashSet<int>>();
+        Queue<int> q = new Queue<int>();
         HashSet<int> visited = new HashSet<int>();
-        Queue<int> available = new Queue<int>();
-
-        foreach(var item in prerequisites) {
-            int item1 = item[0];
-            int item2 = item[1];
-            if(!inDeg.ContainsKey(item1)) {
-                inDeg[item1] = 0;
-            }
-
-            inDeg[item1]++;
-
-            if(!outDeg.ContainsKey(item2)) {
-                outDeg[item2] = new List<int>();
-            }
-
-            outDeg[item2].Add(item1);
-        }
 
         for(int i = 0; i < numCourses; i++) {
-            if(!inDeg.ContainsKey(i)) {
-                available.Enqueue(i);
+            inboundMap[i] = new HashSet<int>();
+        }
+
+        foreach(var item in prerequisites) {
+            var course = item[0];
+            var prerequisite = item[1];
+
+            inboundMap[course].Add(prerequisite);
+
+            if(!outBoundMap.ContainsKey(prerequisite)) {
+                outBoundMap[prerequisite] = new HashSet<int>();
+            }
+
+            outBoundMap[prerequisite].Add(course);
+        }
+
+        foreach(var item in inboundMap) {
+            if(item.Value.Count == 0) {
+                q.Enqueue(item.Key);
             }
         }
 
-        while(available.Count > 0) {
-            var cur = available.Dequeue();
-            if(!visited.Contains(cur)) {
-                visited.Add(cur);
-                if(outDeg.ContainsKey(cur)) {
-                    foreach(var item in outDeg[cur]) {
-                        if(inDeg.ContainsKey(item)) {
-                            if(inDeg[item] == 1) {
-                                inDeg.Remove(item);
-                                available.Enqueue(item);
+        while(q.Count > 0) {
+            var current = q.Dequeue();
+            if(!visited.Contains(current)) {
+                visited.Add(current);
+                if(outBoundMap.ContainsKey(current)) {
+                    foreach(var item in outBoundMap[current]) {
+                        if(inboundMap.ContainsKey(item)) {
+                            if(inboundMap[item].Count == 1) {
+                                q.Enqueue(item);
+                                inboundMap.Remove(item);
                             } else {
-                                inDeg[item]--;
+                                inboundMap[item].Remove(current);
                             }
                         }
                     }
                 }
                 
-            }
+            }            
         }
 
         return visited.Count == numCourses;
